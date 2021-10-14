4a473ea8c2a7e606cb7a2a468f6bd0880075b36c
Ol3ggro <oleg.kravc.75@mail.ru>
2021-10-14 12:30:58 +0400

    ADOQuery

--- UnitPALAT.pas
+++ UnitPALAT.pas
@@ -4,13 +4,17 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, jpeg, ExtCtrls, Grids, DBGrids;
+  Dialogs, jpeg, ExtCtrls, Grids, DBGrids, StdCtrls, DBCtrls;
 
 type
   TFormPALAT = class(TForm)
     Image1: TImage;
     DBGrid1: TDBGrid;
     DBGrid2: TDBGrid;
+    DBNavigator1: TDBNavigator;
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure DBGrid1CellClick(Column: TColumn);
   private
     { Private declarations }
   public
@@ -22,8 +26,19 @@ var
 
 implementation
 
-uses UnitBD;
+uses UnitBD, UnitKlient, UnitMain;
 
 {$R *.dfm}
+
+procedure TFormPALAT.Button1Click(Sender: TObject);
+begin
+close;
+end;
+
+procedure TFormPALAT.DBGrid1CellClick(Column: TColumn);
+begin
+DataBD.ADOQueryPalat.Filtered := true;
+DataBD.ADOQueryPalat.Filtered:='kod_palat' = '''+DataBD.ADOPalat.Fieldbyname('kod_palat').AsString+'''';
+end;
 
 end.
